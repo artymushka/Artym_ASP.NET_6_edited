39cc909798f2882973fe17f8e83ec9b6b39d119b
Vlad_Artym <vladartym1@gmail.com>
2024-03-17 19:32:51 +0200

    add commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitOrder(User user)
+        {
+            if (user.Age >= 16)
+            {
+                TempData["Quantity"] = user.Age;
+                return RedirectToAction("ProductOrder");
+            }
+            else
+            {
+                return Content("Вибачте, ви повинні бути старші за 16 років для замовлення товарів.");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ProductOrder()
+        {
+            if (TempData["Quantity"] == null)
+            {
+                return RedirectToAction("HomePage");
+            }
+            ViewBag.Quantity = TempData["Quantity"];
+            return View(); 
+        }
+
+
+        [HttpPost]
+        public IActionResult ConfirmOrder(Product product)
+        {
+            // Обработка заказа товара
+            return View("ConfirmationPage", product);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WebApplication1/Models/OrderPageViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class OrderPageViewModel
+    {
+        public int Quantity { get; set; }
+    }
+}
